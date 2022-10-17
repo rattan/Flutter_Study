b4e9d4ed8c25203fcff9f33b7ee6dd20e6e05b64
gin <rattan32g@gmail.com>
2022-10-17 20:54:53 +0900

    Basic widgets
    Basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key, required this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 56.0,
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(color: Colors.blue[500]),
+        child: Row(
+          children: <Widget>[
+            const IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation menu',
+              onPressed: null,
+            ),
+            Expanded(child: title),
+            const IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            )
+          ],
+        ));
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+      children: <Widget>[
+        MyAppBar(
+            title: Text('Example title',
+                style: Theme.of(context).primaryTextTheme.titleMedium)),
+        const Expanded(child: Center(child: Text('Hello, world!')))
+      ],
+    ));
+  }
+}
+
 void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
-      ),
-    ),
-  );
+  runApp(const MaterialApp(title: 'My app', home: MyScaffold()));
 }
