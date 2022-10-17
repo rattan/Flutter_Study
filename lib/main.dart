642146bd58af39852170c3d368a9db638d493e60
gin <rattan32g@gmail.com>
2022-10-17 21:40:32 +0900

    Using Material Components
    Using Material Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,36 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({super.key, required this.title});
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 56.0,
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(color: Colors.blue[500]),
-        child: Row(
-          children: <Widget>[
-            const IconButton(
-              icon: Icon(Icons.menu),
-              tooltip: 'Navigation menu',
-              onPressed: null,
-            ),
-            Expanded(child: title),
-            const IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            )
-          ],
-        ));
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-        child: Column(
-      children: <Widget>[
-        MyAppBar(
-            title: Text('Example title',
-                style: Theme.of(context).primaryTextTheme.titleMedium)),
-        const Expanded(child: Center(child: Text('Hello, world!')))
-      ],
-    ));
-  }
-}
-
 void main() {
-  runApp(const MaterialApp(title: 'My app', home: MyScaffold()));
+  runApp(const MaterialApp(title: 'Flutter Tutorial', home: TutorialHome()));
+}
+
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: const Text('Example title'),
+        actions: const <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Hello world!')),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add',
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
