81fba679d28bdf5de64f2ea688351ee32b531722
gin <rattan32g@gmail.com>
2022-10-17 22:22:16 +0900

    Handling gestures
    Handling gestures

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class TutorialHome extends StatelessWidget {
         ),
         title: const Text('Example title'),
         actions: const <Widget>[
+          MyButton(),
           IconButton(
             icon: Icon(Icons.search),
             tooltip: 'Search',
@@ -30,6 +31,29 @@ class TutorialHome extends StatelessWidget {
         tooltip: 'Add',
         onPressed: null,
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(child: Text('Engage')),
       ),
     );
   }
